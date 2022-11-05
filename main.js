fa7b765861af0778c560c0a6bc053a627641db2f
IG <iikim2511@gmail.com>
2022-11-05 00:45:54 -0400

    gitignore

---
+++ main.js
@@ -0,0 +1,6 @@
+const videoCardContainer = document.querySelector('.video-container');
+
+let api_key = google_api_key;
+let video_http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
